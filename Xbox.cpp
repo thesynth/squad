5ed07bc8f85c900212fdbb06dd3088adb9601942
le `water <45738520+le-water@users.noreply.github.com>
2019-05-07 14:30:29 -0400

    Update Xbox.cpp

--- Xbox.cpp
+++ Xbox.cpp
@@ -1,1 +1,41 @@
+auto mpInstance = multiplayer_manager::get_singleton_instance();
 
+mpInstance->initialize(lobbySessionTemplateName);
+
+auto mpInstance = multiplayer_manager::get_singleton_instance();
+string_t connectionAddress = L"1.1.1.1";
+
+for (User^ user : User::Users)
+{
+  if( user->IsSignedIn )
+  {
+    auto result = mpInstance.lobby_session()->add_local_user(user);
+    if (result.err())
+    {
+      // handle error
+    }
+
+    // Set member connection address
+    mpInstance->lobby_session()->set_local_member_connection_address(
+        xboxLiveContext->user(), connectionAddress);
+
+    // Set custom member properties
+    mpInstance->lobby_session()->set_local_member_properties(xboxLivecontext->user(), ..., ...)
+  }
+}
+
+auto result = mpInstance->lobby_session()->invite_friends(xboxLiveContext);
+if (result.err())
+{
+  // handle error
+}
+
+auto result = mpInstance().join_lobby(IProtocolActivatedEventArgs^ args);
+if (result.err())
+{
+  // handle error
+}
+
+string_t connectionAddress = L"1.1.1.1";
+mpInstance->lobby_session()->set_local_member_connection_address(
+    xboxLiveContext->user(), connectionAddress);
