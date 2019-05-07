d3151db3fda698b85934938e923716858bc219e5
le `water <45738520+le-water@users.noreply.github.com>
2019-05-07 14:34:43 -0400

    Update Steam.cpp

--- Steam.cpp
+++ Steam.cpp
@@ -1,1 +1,15 @@
-
+IOnlineSubsystem* OnlineSub = IOnlineSubsystem::Get(); 
+ // Retrieve our Interface
+ if (OnlineSub) // if an Online Interface is valid
+ {
+     IOnlineSessionPtr SessionInt = OnlineSub->GetSessionInterface();
+     // Get the Session Interface Pointer
+     FOnSessionInviteAcceptedDelegate OnSessionInviteAcceptedDelegate = FOnSessionInviteAcceptedDelegate::CreateUObject(this, &MyClass:MyCallbackFunction); 
+     // Create a Delegate For Session Invite Acceptance. 
+     //This will trigger our function Whenever we accept an Invite 
+     SessionInt->AddOnSessionInviteAcceptedDelegate_Handle(0, OnSessionInviteAcceptedDelegate); 
+     // Bind the Delegate
+ }
+ 
+ MyCallbackFunction(int32 LocalUserNum, bool bWasSuccessful, const FOnlineSessionSearchResult & SessionToJoin)
+ 
