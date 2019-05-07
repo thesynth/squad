9b2fcf43f790198789fc18ffc39749084600c70d
le `water <45738520+le-water@users.noreply.github.com>
2019-05-07 15:32:53 -0400

    Update Windows-10-Microsoft-store.cpp

--- Windows-10-Microsoft-store.cpp
+++ Windows-10-Microsoft-store.cpp
@@ -1,1 +1,17 @@
+#include <bits/stdc++.h>
 
+
+auto result = mpInstance->lobby_session()->invite_friends(xboxLiveContext);
+if (result.err())
+{
+  // handle error
+}
+
+std::vector<string_t>& xboxUserIds;
+xboxUserIds.push_back();  // Add xbox_user_ids from your in-game roster list
+
+auto result = mpInstance->lobby_session()->invite_users(xboxUserIds);
+if (result.err())
+{
+  // handle error
+}
